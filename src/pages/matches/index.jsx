58bf2ab606949e9cd342b207df31b01614d5402f
mrubio7 <manuel.rubio.arias@gmail.com>
2023-12-15 13:26:05 +0100

    v1.0.0

--- src/pages/matches/index.jsx
+++ src/pages/matches/index.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import MatchList from "../../components/match-list";
+import Context from "../../context";
+import { texts } from "../../utils/translate";
 
 const Matches = () => {
+    const obj = useContext(Context);
+    document.title = texts[obj.Lang].TITLE_MATCHES;
+
     return (
         <>
             <MatchList isIndex={false}/>
